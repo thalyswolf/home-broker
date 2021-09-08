a0a6a98564233d2fdb4d0a10779410f1aa252e40
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-07 21:28:44 -0300

    add get stocks by ticket

--- src/core/usecase/__init__.py
+++ src/core/usecase/__init__.py
@@ -1,1 +1,2 @@
 from src.core.usecase.list_stocks import ListStocks
+from src.core.usecase.get_stocks_by_ticket import GetStocksByTicket
---
+++ src/core/usecase/get_stocks_by_ticket.py
@@ -0,0 +1,11 @@
+from src.contracts.providers import StocksProvidersContract
+from src.core.entity import Stocks
+
+
+class GetStocksByTicket:
+
+    def __init__(self, stocks_provider: StocksProvidersContract):
+        self.stocks_provider = stocks_provider
+
+    def execute(self, ticket) -> Stocks:
+        return self.stocks_provider.get_stock_by_ticket(ticket=ticket)
