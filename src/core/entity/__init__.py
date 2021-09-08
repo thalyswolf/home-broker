08b2739ec6d23d7910bced00ffd266a9838aad16
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-07 22:45:21 -0300

    add StockOffer entity

--- src/core/entity/__init__.py
+++ src/core/entity/__init__.py
@@ -1,1 +1,2 @@
 from src.core.entity.stocks import *
+from src.core.entity.stocks_offer import *
---
+++ src/core/entity/stocks_offer.py
@@ -0,0 +1,6 @@
+class StocksOffer:
+    _id: str
+    ticket: str
+    price_sell: str
+    price_buy: str
+    expire_timestamp: int
