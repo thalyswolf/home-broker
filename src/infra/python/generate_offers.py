e088a83784d5fe7f8c77e5d7fad0fa23990bf800
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-07 23:29:47 -0300

    refactor: dependency injection in call of usecase

--- src/infra/python/generate_offers.py
+++ src/infra/python/generate_offers.py
@@ -1,11 +1,13 @@
+from src.infra.message.stocks_offer_memory_message import StocksOfferMemoryMessage
 from src.infra.providers.memory_stocks.stocks import MemoryStocksProvider
 from src.infra.repository import StocksOfferMemoryRepository
 from src.core.usecase import GenerateOffers
 
 memory_stocks_provider = MemoryStocksProvider()
 stocks_offer_memory_repository = StocksOfferMemoryRepository()
+stocks_offer_memory_message = StocksOfferMemoryMessage()
 
-stocks = GenerateOffers(memory_stocks_provider, stocks_offer_memory_repository).execute()
+stocks = GenerateOffers(memory_stocks_provider, stocks_offer_memory_repository, stocks_offer_memory_message).execute()
 
 for item in stocks:
     print(item.__dict__)
