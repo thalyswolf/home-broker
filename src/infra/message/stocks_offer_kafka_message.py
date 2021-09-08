d279d92828388412257e88684504668a0d007ed3
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-08 20:26:00 -0300

    add listener kafka

--- src/infra/message/stocks_offer_kafka_message.py
+++ src/infra/message/stocks_offer_kafka_message.py
@@ -1,4 +1,4 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaConsumer
 
 from src.core.entity import StocksOffer
 from src.contracts.message import StocksOfferMessageContract
@@ -8,12 +8,23 @@ from src.infra.kafka.connection import KafkaConnection
 class StocksOfferKafkaMessage(StocksOfferMessageContract):
 
     _producer: KafkaProducer
+    _consumer: KafkaConsumer
 
     def __init__(self):
-        self._producer = KafkaConnection().get_producer()
+        connection = KafkaConnection()
+        self._producer = connection.get_producer()
+        self._consumer = connection.get_consumer()
+
 
 
     def send_offers_notifications(self, stock_offer: StocksOffer) -> None:
         self._producer.send('offers', stock_offer.__dict__)
         self._producer.flush()
         print("Notified by Kafka")
+
+    def listening_offers(self):
+        self._consumer.subscribe(['offers'])
+
+        for msg in self._consumer:
+            print('Consuming in Kafka')
+            print(msg.value)
