2a63816c917faebe19d48e582c29816e16cc9eb9
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-07 23:00:08 -0300

    Generate Offer test

---
+++ src/test/generate_offer_test.py
@@ -0,0 +1,27 @@
+from unittest import TestCase, main
+
+from src.infra.providers.memory_stocks.stocks import MemoryStocksProvider
+from src.infra.repository import StocksOfferMemoryRepository
+from src.core.usecase import GenerateOffers
+
+class GenerateOfferTest(TestCase):
+
+    def test_insert_offer_in_db(self):
+        ticket = 'ABEV3'
+        memory_stocks_provider = MemoryStocksProvider()
+        stocks_offer_memory_repository = StocksOfferMemoryRepository()
+
+        stocks = GenerateOffers(memory_stocks_provider, stocks_offer_memory_repository).execute()
+        TestCase().assertTrue(isinstance(stocks, list))
+
+
+        stock = stocks_offer_memory_repository.get_stock_by_ticket(ticket)
+
+        TestCase().assertEqual(stock.ticket, ticket)
+        TestCase().assertTrue(isinstance(stock.price_buy, float))
+        TestCase().assertTrue(isinstance(stock.price_sell, float))
+        TestCase().assertTrue(isinstance(stock._id, str))
+        TestCase().assertTrue(isinstance(stock.expire_timestamp, int))
+
+if __name__ == '__main__':
+    main()
