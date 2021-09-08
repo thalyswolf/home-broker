116e66185234bc2893d79d1a576bdbd1560dda50
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-07 21:17:56 -0300

    add first entity

---
+++ src/core/entity/__init__.py
@@ -0,0 +1,1 @@
+from src.core.entity.stocks import *
---
+++ src/core/entity/stocks.py
@@ -0,0 +1,14 @@
+class Stocks:
+    _id: str
+    company_name: str
+    ticket: str
+    line_of_business: str
+    price_sell: str
+    price_buy: str
+
+    def __init__(self, company_name, ticket, line_of_business, price_sell, price_buy) -> None:
+        self.company_name = company_name
+        self.ticket = ticket
+        self.line_of_business = line_of_business
+        self.price_sell = price_sell
+        self.price_buy = price_buy
